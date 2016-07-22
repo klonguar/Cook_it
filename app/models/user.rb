06784e7c575d669f18172470f7227a74eb89f041
klonguar <klonguar@gmail.com>
2016-07-22 21:59:08 +0000

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
-    validates :username, presence: true, length: {minimum: 3, maximum: 50}
-    validates :email, presence: true, length: {minimum: 10, maximum: 300}
+    validates :username, presence: true, 
+                         uniqueness: { case_sensitive: false }, 
+                         length: {minimum: 3, maximum: 50}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+    validates :email, presence: true, 
+                      length: {maximum: 105},
+                      uniqueness: { case_sensitive: false }, 
+                      format: { with: VALID_EMAIL_REGEX }
 end
+
+#VALID_EMAIL_REGEX code is for email validation , all code is from Michael Hartl Tutorial, Source: https://www.natashatherobot.com/ruby-email-validation-regex/
