7a8608402a4403216c97d8ba8c3142a4869955ca
klonguar <klonguar@gmail.com>
2016-07-25 22:45:20 +0000

    build login page and session view and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   get 'signup', to: 'users#new' 
 
-  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   
   get 'pages/about', to: 'pages#about'
