de0717a6b982949a8ffafe8d38d38091387b490d
klonguar <klonguar@gmail.com>
2016-07-25 18:43:09 +0000

    Add users listing and update show recipe view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def index
+    @users = User.all
+  end
     
   def show
     @user = User.find(params[:id])
