818f24b886e0efb9390a66d219f1c21c43c6b4cb
klonguar <klonguar@gmail.com>
2016-07-28 21:48:41 +0000

    updated maximum length for recipes description

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ActiveRecord::Base
                       length: {minimum: 3, maximum: 50}
     
     validates :description, presence: true, 
-                            length: {minimum: 10, maximum: 1000}
+                            length: {minimum: 10, maximum: 10000}
     
     validates :user_id, presence: true #this will ensure every time the recipe is create the user_id is present
 end
